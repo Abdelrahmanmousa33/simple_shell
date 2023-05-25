c5ed14b8d60f96af4b85804a9fc5245c5f67bb3f
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-05-25 09:34:04 -0700

    fixed a bug in exit builtin

--- _execve.c
+++ _execve.c
@@ -9,5 +9,8 @@
 void _execve(char *argv[], char **av, char **env)
 {
 	if (execve(argv[0], argv, env) == -1)
+	{
 		printf("%s:%s:no such file or directory\n", av[0], argv[0]);
+		exit(0);
+	}
 }
--- builtin.c
+++ builtin.c
@@ -10,7 +10,7 @@ void builtin(char *command[], char *buffer)
 	if (strcmp(command[0], "exit") == 0)
 	{
 		free(buffer);
-		exit(EXIT_SUCCESS);
+		exit(2);
 	}
 	if (strcmp(command[0], "env") == 0)
 	{
